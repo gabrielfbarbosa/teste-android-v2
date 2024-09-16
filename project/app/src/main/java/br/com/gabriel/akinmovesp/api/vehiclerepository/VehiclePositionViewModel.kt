95afb8411d7924634008085691dd86ec7227cbfd
gabrielfbarbosa <gfelipebarbosa@yahoo.com>
2024-09-16 01:24:59 -0300

    Implemented automatic data refresh

--- project/app/src/main/java/br/com/gabriel/akinmovesp/api/vehiclerepository/VehiclePositionViewModel.kt
+++ project/app/src/main/java/br/com/gabriel/akinmovesp/api/vehiclerepository/VehiclePositionViewModel.kt
@@ -1,10 +1,10 @@
-package br.com.gabriel.akinmovesp.api.estimatedrepository
+package br.com.gabriel.akinmovesp.api.vehiclerepository
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import br.com.gabriel.akinmovesp.api.models.estimatedmodel.EstimatedResponseModel
+import br.com.gabriel.akinmovesp.api.models.vehiclemodel.PositionResponseModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -13,34 +13,33 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class EstimatedViewModel @Inject constructor(
-    private val getPrevisaoUseCase: GetEstimatedUseCase
+class VehiclePositionViewModel @Inject constructor(
+    private val getPosicoesVehicleUseCase: GetPositionsVehicleUseCase
 ) : ViewModel() {
 
-    private val _estimated = MutableLiveData<EstimatedResponseModel?>()
-    val estimated: LiveData<EstimatedResponseModel?> = _estimated
+    private val _positionsVehicles = MutableLiveData<PositionResponseModel?>()
+    val vehiclesPositions: LiveData<PositionResponseModel?> = _positionsVehicles
 
-    private val _isLoading = MutableLiveData<Boolean>(false)
+    private val _isLoading = MutableLiveData<Boolean>(true)
     val isLoading: LiveData<Boolean> = _isLoading
 
     private val _errorMessage = MutableLiveData<String?>()
     val errorMessage: LiveData<String?> = _errorMessage
 
     private var refreshJob: Job? = null
-    private val refreshIntervalMillis: Long = 20000 // 20 segundos
+    private val refreshIntervalMillis: Long = 5000 // 5 segundos
 
-    fun setErrorMessage(message: String?) {
-        _errorMessage.value = message
-        _estimated.value = null
+    init {
+        startAutomaticRefresh()
     }
 
-    internal fun startAutomaticRefresh(stopCode: Int) {
+    private fun startAutomaticRefresh() {
         // Evita múltiplas chamadas
         if (refreshJob?.isActive == true) return
 
         refreshJob = viewModelScope.launch {
             while (isActive) {
-                getEstimated(stopCode)
+                getPositionsVehicles()
                 delay(refreshIntervalMillis)
             }
         }
@@ -51,14 +50,11 @@ class EstimatedViewModel @Inject constructor(
         refreshJob = null
     }
 
-    fun getEstimated(stopCode: Int) {
+    fun getPositionsVehicles() {
         viewModelScope.launch {
-            _isLoading.value = true
-            _errorMessage.value = null  // Limpar a mensagem de erro antes de iniciar a busca
-            _estimated.value = null  // Limpar os dados antes de iniciar a busca
-            val result = getPrevisaoUseCase(stopCode)
-            result.onSuccess { estimatedResponse ->
-                _estimated.value = estimatedResponse
+            val result = getPosicoesVehicleUseCase()
+            result.onSuccess { positions ->
+                _positionsVehicles.value = positions
                 _isLoading.value = false
             }.onFailure { error ->
                 _errorMessage.value = error.message
